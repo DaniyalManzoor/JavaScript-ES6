dfeab10eb29fcaa8a3c094068a72bf96860df9c2
Daniyal Manzoor <daniyalmanzoor28@gmail.com>
2020-09-13 06:46:22 -0700

    spread operator using object

---
+++ 09 spread operator with object.js
@@ -0,0 +1,7 @@
+const first = { name: "Daniyal" };
+const second = { job: "React Developer" };
+
+const combined = { ...first, ...second, name: "pakistan" };
+
+//clone;
+const clone = { ...first };
