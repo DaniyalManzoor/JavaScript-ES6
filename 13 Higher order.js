1d6d550b40ca90d9bdc6a0a94f111b7f8bd97e0e
Daniyal Manzoor <daniyalmanzoor28@gmail.com>
2020-09-14 21:48:36 -0700

    Higher order function

---
+++ 13 Higher order.js
@@ -0,0 +1,10 @@
+// Higher order function
+
+// Higher order function are the function that take a
+// function as an argument or return it or both.
+
+let numbers = [1, 2, 3];
+let product = numbers.map((number) => number * 2);
+
+//after one second
+setTimeout(() => console.log("Hello world"), 1000);
